9a98cae995057953e3dd17dc79f361ca8d01959c
eggyo <eggyoyo_nawf@hotmail.com>
2016-07-04 16:54:12 +0700

    Update main.go
    add Floattostring func

--- main.go
+++ main.go
@@ -103,3 +103,7 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 }
+func FloatToString(input_num float64) string {
+    // to convert a float number to a string
+    return strconv.FormatFloat(input_num, 'f', 6, 64)
+}
