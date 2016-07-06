44547c2ccb981ced8e22e1bce7f2d58daff96452
eggyo <eggyoyo_nawf@hotmail.com>
2016-07-06 22:23:08 +0700

    Update main.go

--- main.go
+++ main.go
@@ -67,6 +67,7 @@ func main() {
 }
    
 func callbackHandler(w http.ResponseWriter, r *http.Request) {
+        Var geo ResultGeoLoc
 	received, err := bot.ParseRequest(r)
 	if err != nil {
 		if err == linebot.ErrInvalidSignature {
