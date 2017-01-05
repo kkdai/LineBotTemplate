2e185329a0e005f037ed99164db1cdc0916959af
jinhsueh <hsuehchin76@gmail.com>
2017-01-05 14:19:57 +0800

    Update main.go

--- main.go
+++ main.go
@@ -13,140 +13,46 @@
 package main
 
 import (
-    "bytes"
-    "encoding/json"
-    "fmt"
-    "io/ioutil"
-    "log"
-    "net/http"
-    "net/url"
-    "os"
-    "time"
-    "github.com/line/line-bot-sdk-go/linebot"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/line/line-bot-sdk-go/linebot"
 )
 
-// ReceivedMessage ...
-type ReceivedMessage struct {
-    Result []Result `json:"result"`
-}
-
-// Result ...
-type Result struct {
-    ID          string   `json:"id"`
-    From        string   `json:"from"`
-    FromChannel int      `json:"fromChannel"`
-    To          []string `json:"to"`
-    ToChannel   int      `json:"toChannel"`
-    EventType   string   `json:"eventType"`
-    Content     Content  `json:"content"`
-}
-
-// SendMessage ..
-type SendMessage struct {
-    To        []string `json:"to"`
-    ToChannel int      `json:"toChannel"`
-    EventType string   `json:"eventType"`
-    Content   Content  `json:"content"`
-}
-
-// Content ...
-type Content struct {
-    ID          string   `json:"id"`
-    ContentType int      `json:"contentType"`
-    From        string   `json:"from"`
-    CreatedTime int      `json:"createdTime"`
-    To          []string `json:"to"`
-    ToType      int      `json:"toType"`
-    Text        string   `json:"text"`
-}
-
-// const ...
-const (
-    EndPoint  = "https://trialbot-api.line.me"
-    ToChannel = 1383378250
-    EventType = "138311608800106203"
-)
+var bot *linebot.Client
 
 func main() {
-    http.HandleFunc("/", helloHandler)
-    http.HandleFunc("/callback", callbackHandler)
-    port := os.Getenv("PORT")
-    addr := fmt.Sprintf(":%s", port)
-    http.ListenAndServe(addr, nil)
-}
-
-func helloHandler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hello, LINE Bot")
+	var err error
+	bot, err = linebot.New(os.Getenv("ChannelSecret"), os.Getenv("ChannelAccessToken"))
+	log.Println("Bot:", bot, " err:", err)
+	http.HandleFunc("/callback", callbackHandler)
+	port := os.Getenv("PORT")
+	addr := fmt.Sprintf(":%s", port)
+	http.ListenAndServe(addr, nil)
 }
 
 func callbackHandler(w http.ResponseWriter, r *http.Request) {
-    decoder := json.NewDecoder(r.Body)
-    var m ReceivedMessage
-    err := decoder.Decode(&m)
-    if err != nil {
-        log.Println(err)
-    }
-    apiURI := EndPoint + "/v1/events"
-    for _, result := range m.Result {
-        from := result.Content.From
-        text := result.Content.Text
-        content := new(Content)
-        content.ContentType = result.Content.ContentType
-        content.ToType = result.Content.ToType
-        content.Text = text
-        request(apiURI, "POST", []string{from}, *content)
-    }
+	events, err := bot.ParseRequest(r)
+
+	if err != nil {
+		if err == linebot.ErrInvalidSignature {
+			w.WriteHeader(400)
+		} else {
+			w.WriteHeader(500)
+		}
+		return
+	}
+
+	for _, event := range events {
+		if event.Type == linebot.EventTypeMessage {
+			switch message := event.Message.(type) {
+			case *linebot.TextMessage:
+				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.ID+":"+message.Text+" OK!+ fuck")).Do(); err != nil {
+					log.Print(err)
+				}
+			}
+		}
+	}
 }
-
-func request(endpointURL string, method string, to []string, content Content) {
-    m := &SendMessage{}
-    m.To = to
-    m.ToChannel = ToChannel
-    m.EventType = EventType
-    m.Content = content
-    b, err := json.Marshal(m)
-    if err != nil {
-        log.Print(err)
-    }
-    req, err := http.NewRequest(method, endpointURL, bytes.NewBuffer(b))
-    if err != nil {
-        log.Print(err)
-    }
-    req = setHeader(req)
-    client := &http.Client{
-        Transport: &http.Transport{Proxy: http.ProxyURL(getProxyURL())},
-        Timeout:   time.Duration(30 * time.Second),
-    }
-    res, err := client.Do(req)
-    if err != nil {
-        log.Print(err)
-    }
-    defer res.Body.Close()
-
-    var result map[string]interface{}
-    body, err := ioutil.ReadAll(res.Body)
-    if err != nil {
-        log.Print(err)
-    }
-    if err := json.Unmarshal(body, &result); err != nil {
-        log.Print(err)
-    }
-    log.Print(result)
-}
-
-func setHeader(req *http.Request) *http.Request {
-    req.Header.Add("Content-Type", "application/json; charset=UTF-8")
-    req.Header.Add("X-Line-ChannelID", os.Getenv("ChannelID"))
-    req.Header.Add("X-Line-ChannelSecret", os.Getenv("ChannelSecret"))
-    req.Header.Add("X-Line-Trusted-User-With-ACL", os.Getenv("MID"))
-    return req
-}
-
-func getProxyURL() *url.URL {
-    proxyURL, err := url.Parse(os.Getenv("ProxyURL"))
-    if err != nil {
-        log.Print(err)
-    }
-    return proxyURL
-}
-
