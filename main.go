0599810b6cd30eafbafa7434bc1a7824ee53a400
pojulee <poju0424@gmail.com>
2017-03-07 14:33:58 +0800

    tt

--- main.go
+++ main.go
@@ -75,7 +75,7 @@ func sqlConnect(currency string)(output string){
 	checkErr(err)
 	
 	// rows, err := db.Query("SELECT * FROM $1 ORDER BY id DESC LIMIT 1;", currency)
-	rows, err := db.Query("SELECT * FROM "+currency+" ORDER BY id DESC LIMIT 1;")
+	rows, err := db.Query("SELECT id, cashbuy FROM "+currency+" ORDER BY id DESC LIMIT 1;")
 	checkErr(err)
 	defer rows.Close()
 	for rows.Next(){
