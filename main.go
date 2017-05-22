0281a54ce39eba4260f3a72ed96bf7d796ea2cb0
zchien <zchien@gmail.com>
2017-05-22 17:37:18 +0800

    no message

--- main.go
+++ main.go
@@ -10,7 +10,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 // https://github.com/line/line-bot-sdk-go/tree/master/linebot
-e
+
 package main
 
 import (
