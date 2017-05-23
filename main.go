d62ece0c6e2fbbe67c12ee7a82b0d2d1069e5dd7
zchien <zchien@gmail.com>
2017-05-23 09:41:29 +0800

    no message

--- main.go
+++ main.go
@@ -31,18 +31,19 @@ var echoMap = make(map[string]bool);
 var bot *linebot.Client
 
 
-func tellTime(event *linebot.Event, timeString string){
+func tellTime(replyToken string, timeString string){
 	if timeString == "" {
-		bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("現在時間是: " + time.Now().Format("2006-01-02 15:04:05"))).Do();
+		bot.ReplyMessage(replyToken, linebot.NewTextMessage("現在時間是: " + time.Now().Format("2006-01-02 15:04:05"))).Do();
 	} else if silent != true {
-		bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("自動報時: " + timeString)).Do();
+		bot.ReplyMessage(replyToken, linebot.NewTextMessage("自動報時: " + timeString)).Do();
 	}				
 }
 
-func routineDog(event *linebot.Event) {
+func routineDog(replyToken String) {
 	for {
 		time.Sleep(15 * 60 * 1000 * time.Millisecond) //time.Sleep(100 * time.Millisecond)
-		tellTime(event, time.Now().Format("2006-01-02 15:04:05"));
+		log.Println("time to tell time to : " + replyToken + ", " + time.Now().Format("2006-01-02 15:04:05"))
+		tellTime(replyToken, time.Now().Format("2006-01-02 15:04:05"));
 	}
 }
 
@@ -93,7 +94,7 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	for _, event := range events {
-	
+		var replyToken = event.ReplyToken
 		var sourceId = getSourceId(event)
 		log.Print("callbackHandler to source id: " + sourceId)
 
@@ -111,7 +112,7 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 			switch message := event.Message.(type) {
 			case *linebot.TextMessage:
 				log.Print("TextMessage: ID(" + message.ID + "), Text(" + message.Text  + "), current silent status=" + strconv.FormatBool(silent) )
-				//if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.ID+":"+message.Text+" OK!")).Do(); err != nil {
+				//if _, err = bot.ReplyMessage(replyToken, linebot.NewTextMessage(message.ID+":"+message.Text+" OK!")).Do(); err != nil {
 				//	log.Print(err)
 				//}
 				
@@ -119,39 +120,39 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 				
 				if strings.ContainsAny(message.Text, "你閉嘴") {
 					silent = true;
-					bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("QQ")).Do();
+					bot.ReplyMessage(replyToken, linebot.NewTextMessage("QQ")).Do();
 				} else if "說吧" == message.Text {
 					silent = false;
-					bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("麥克風測試，1、2、3... OK")).Do();
+					bot.ReplyMessage(replyToken, linebot.NewTextMessage("麥克風測試，1、2、3... OK")).Do();
 				} else if strings.ContainsAny(message.Text, "現在幾點")  {
-					tellTime(event, "");
+					tellTime(replyToken, "");
 				} else if silent != true {
-					bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("嗯嗯，呵呵，我要去洗澡了")).Do();
+					bot.ReplyMessage(replyToken, linebot.NewTextMessage("嗯嗯，呵呵，我要去洗澡了")).Do();
 				}
 			case *linebot.ImageMessage :
 				log.Print("ImageMessage: ID(" + message.ID + "), OriginalContentURL(" + message.OriginalContentURL + "), PreviewImageURL(" + message.PreviewImageURL + ")" )
 				if silent != true {
-					bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("傳這甚麼廢圖？你有認真在分享嗎？")).Do();
+					bot.ReplyMessage(replyToken, linebot.NewTextMessage("傳這甚麼廢圖？你有認真在分享嗎？")).Do();
 				}
 			case *linebot.VideoMessage :
 				log.Print("VideoMessage: ID(" + message.ID + "), OriginalContentURL(" + message.OriginalContentURL + "), PreviewImageURL(" + message.PreviewImageURL + ")" )
 				if silent != true {
-					bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("看甚麼影片，不知道流量快用光了嗎？")).Do();
+					bot.ReplyMessage(replyToken, linebot.NewTextMessage("看甚麼影片，不知道流量快用光了嗎？")).Do();
 				}
 			case *linebot.AudioMessage :
 				log.Print("AudioMessage: ID(" + message.ID + "), OriginalContentURL(" + message.OriginalContentURL + "), Duration(" + strconv.Itoa(message.Duration) + ")" )
 				if silent != true {
-					bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("說的比唱的好聽，唱得鬼哭神號，是要嚇唬誰？")).Do();
+					bot.ReplyMessage(replyToken, linebot.NewTextMessage("說的比唱的好聽，唱得鬼哭神號，是要嚇唬誰？")).Do();
 				}
 			case *linebot.LocationMessage:
 				log.Print("LocationMessage: ID(" + message.ID + "), Title(" + message.Title  + "), Address(" + message.Address + "), Latitude(" + strconv.FormatFloat(message.Latitude, 'f', -1, 64) + "), Longitude(" + strconv.FormatFloat(message.Longitude, 'f', -1, 64) + ")" )
 				if silent != true {
-					bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("這是哪裡啊？火星嗎？")).Do();
+					bot.ReplyMessage(replyToken, linebot.NewTextMessage("這是哪裡啊？火星嗎？")).Do();
 				}
 			case *linebot.StickerMessage :
 				log.Print("StickerMessage: ID(" + message.ID + "), PackageID(" + message.PackageID + "), StickerID(" + message.StickerID + ")" )
 				if silent != true {
-					bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("腳踏實地打字好嗎？傳這甚麼貼圖？")).Do();
+					bot.ReplyMessage(replyToken, linebot.NewTextMessage("腳踏實地打字好嗎？傳這甚麼貼圖？")).Do();
 				}
 			}
 		} else if event.Type == linebot.EventTypePostback {
