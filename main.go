d86b34983fee9fceab0b430db34a967d0d6842b2
zchien <zchien@gmail.com>
2017-07-03 15:21:53 +0800

    no message

--- main.go
+++ main.go
@@ -187,9 +187,7 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 					bot.ReplyMessage(replyToken, linebot.NewTextMessage("麥克風測試，1、2、3... OK")).Do()
 				} else if "profile" == message.Text {
 					if source.UserID != "" {
-						if profile, err := bot.GetProfile(source.UserID).Do(); err != nil {
-								log.Print(err)
-						}
+						profile, err := bot.GetProfile(source.UserID).Do()
 						if _, err := bot.ReplyMessage(
 							replyToken,
 							linebot.NewTextMessage("Display name: "+profile.DisplayName + ", Status message: "+profile.StatusMessage)).Do(); err != nil {
