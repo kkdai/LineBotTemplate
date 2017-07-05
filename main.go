7e0e637decd2105eecf3974f963e8aa62b4b3aab
andywei1996 <andy1996andy@gmail.com>
2017-07-05 21:30:18 +0800

    Update main.go

--- main.go
+++ main.go
@@ -49,7 +49,7 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 		if event.Type == linebot.EventTypeMessage {
 			switch message := event.Message.(type) {
 				case *linebot.TextMessage:
-					if message.text == "幹",err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("幹三小啦？")).Do();
+					if message.text == "幹",err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("幹三小啦？")).Do()
 					else,err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("您好:"+message.Text+" 幹!")).Do();
 					err != nil {
 						log.Print(err)
