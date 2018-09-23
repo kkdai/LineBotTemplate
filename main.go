70ab209ef1f24eb5d21b8005f5db1602c2bad44c
kamisamatenshi <tsecm234@gmail.com>
2018-09-24 01:53:23 +0800

    Update main.go

--- main.go
+++ main.go
@@ -207,7 +207,7 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 		
 		if event.Type == linebot.EventTypeMessage {
 			_, silent := silentMap[sourceId]
-			   wash := washMap[sourceId]
+			_, wash := washMap[sourceId]
 			switch message := event.Message.(type) {
 			case *linebot.TextMessage:
 
