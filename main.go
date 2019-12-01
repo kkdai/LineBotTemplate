0614db5b318a4686967c5ba8d12beac852aeab17
Max Zheng <34936931+54bp6cl6@users.noreply.github.com>
2019-12-01 13:46:52 +0800

    Update main.go

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"strconv"
+	//"strconv"
 
 	"github.com/line/line-bot-sdk-go/linebot"
 )
@@ -54,7 +54,7 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 				if err != nil {
 					log.Println("Quota err:", err)
 				}
-				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.ID+":"+message.Text+" OK! remain message:"+strconv.FormatInt(quota.Value, 10))).Do(); err != nil {
+				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.Text+)).Do(); err != nil {
 					log.Print(err)
 				}
 			}
