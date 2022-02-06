6744555be49cf1cdb4c1cbb6b95b557d0168773e
Evan Lin <evan.if.lin@linecorp.com>
2022-02-06 18:52:52 +0800

    Add more help and add sticker msg event

--- main.go
+++ main.go
@@ -49,12 +49,23 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 	for _, event := range events {
 		if event.Type == linebot.EventTypeMessage {
 			switch message := event.Message.(type) {
+			// Handle only on text message
 			case *linebot.TextMessage:
+				// GetMessageQuota: Get how many remain free tier push message quota you still have this month. (maximum 500)
 				quota, err := bot.GetMessageQuota().Do()
+
 				if err != nil {
 					log.Println("Quota err:", err)
 				}
+				// message.ID: Msg unique ID
+				// message.Text: Msg text
 				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.ID+":"+message.Text+" OK! remain message:"+strconv.FormatInt(quota.Value, 10))).Do(); err != nil {
+					log.Print(err)
+				}
+
+			// Handle only on Sticker message
+			case *linebot.StickerMessage:
+				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("sticker")).Do(); err != nil {
 					log.Print(err)
 				}
 			}
